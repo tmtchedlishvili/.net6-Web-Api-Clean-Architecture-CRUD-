0db6283653a8c33337e84b543b6bc310a75b72c3
Tamar <tmtchedlishvili@aldagi.ge>
2022-12-15 15:20:50 +0400

    Added AldagiTestProject.BlazorWASM.csproj

---
+++ AldagiTestProject.BlazorWasm/Program.cs
@@ -0,0 +1,13 @@
+using AldagiTestProject.BlazorWasm;
+using Infrastructure;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddPersistence(builder.Configuration);
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
--- AldagiTestProject.Infrastructure/DB/ApplicationDbContext.cs
+++ AldagiTestProject.Infrastructure/DB/ApplicationDbContext.cs
@@ -9,6 +9,10 @@ public class ApplicationDbContext : DbContext, IApplicationDBContext
  
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
+    public ApplicationDbContext()
+    {
+    }
+
     public DbSet<AppSetting> AppSettings { get; set; }
     public DbSet<Person> Persons { get; set; }
     public DbSet<Country> Countries { get; set; }
